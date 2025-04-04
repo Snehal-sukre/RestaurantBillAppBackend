28434895486f70a6635bcf8732a58088bee4ba1d
Snehal-sukre <snehalsukre15@gmail.com>
2025-04-04 14:57:02 +0530

    Category Added
    Create REST API to Add category to database table

--- RestaurantBillApp/src/main/java/com/example/demo/repository/CategoryRepository.java
+++ RestaurantBillApp/src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -1,19 +1,53 @@
 package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
+
 import com.example.demo.model.*;
 
 @Repository("catRepo")
 public class CategoryRepository {
+	
+	List<Category> list;
 	
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
 	public boolean isAddNewCategory(Category cat)
 	{
-		return false;
+		int value=jdbcTemplate.update("insert into category values('0', ?)", new PreparedStatementSetter()
+				{
+					@Override
+					public void setValues(PreparedStatement ps) throws SQLException {
+						ps.setString(1, cat.getName());
+					}
+			
+				});
+		return value>0?true:false;
+	}
+	
+	public List<Category> viewAllCategory()
+	{
+		list =jdbcTemplate.query("select *from category", new RowMapper<Category>()
+				{
+					@Override
+					public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Category cat=new Category();
+						cat.setId(rs.getInt(1));
+						cat.setName(rs.getString(2));
+						return cat;
+					}
+				});
+		return list;
 	}
 
 }
--- RestaurantBillApp/src/main/java/com/example/demo/service/CategoryService.java
+++ RestaurantBillApp/src/main/java/com/example/demo/service/CategoryService.java
@@ -1,7 +1,12 @@
 package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.model.Category;
 import com.example.demo.repository.*;
 
 
@@ -9,5 +14,15 @@ import com.example.demo.repository.*;
 public class CategoryService {
 	
 	@Autowired
-	CategoryRepository catRepo;
+	private CategoryRepository catRepo;
+	
+	public boolean isAddNewCategory(Category category)
+	{
+		return catRepo.isAddNewCategory(category);
+	}
+	
+	public List<Category> viewAllCategory()
+	{
+		return catRepo.viewAllCategory();
+	}
 }
